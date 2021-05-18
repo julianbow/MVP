ed65653c71df764f5004de12379f04ebf200920c
Julian Bowman <julianfbowman@gmail.com>
2021-05-18 13:48:37 -0600

    added google maps

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,14 +5,15 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        float: 'left'
+    },
     paper: {
       marginTop: theme.spacing(8),
       display: 'flex',
@@ -24,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: '100%', // Fix IE 11 issue.
+      width: '100%',
       marginTop: theme.spacing(1),
     },
     submit: {
@@ -53,18 +54,7 @@ const App = () => {
     }
 
     return (
-        // <div>
-        // <h1>Soaked</h1>
-        //     <form>
-        //         <label>Search Hot Springs in your area (ex: 'AK'): </label>
-        //         <input type='text' onChange={handleChange}></input>
-        //         <button onClick={search}>Search</button>
-        //     </form>
-        //     <div>
-        //         <SpringList springList={springList}/>
-        //     </div>
-        // </div>
-        <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="xs" className={classes.root}>
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
--- client/src/components/SpringList.jsx
+++ client/src/components/SpringList.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import HotTubRoundedIcon from '@material-ui/icons/HotTubRounded';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+    },
+  }));
 
 const SpringList = ({ springList }) => {
+    const classes = useStyles();
+
     if (springList.length !== 0) {
         return (
-            <ul className="springs">
+            <List className={classes.root}>
                 <h3>Hot Springs:</h3>
                 {springList.map((spring) => {
                     return (
-                    <li key={spring.id}>
-                        <span>{spring.name === 'null' ? spring.name = 'Not Named' : spring.name}</span>
-                    </li>
+                    <ListItem key={spring.id} button>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <HotTubRoundedIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText>{spring.name === 'null' ? spring.name = 'Not Named' : spring.name}</ListItemText>
+                    </ListItem>
                     );
                 })}
-            </ul>
+            </List>
         );
     } else {
         return '';
