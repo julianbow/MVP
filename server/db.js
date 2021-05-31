4b424b68d8d9991a38aca1531f36c2d67d19d405
Julian Bowman <julianfbowman@gmail.com>
2021-05-31 10:27:49 -0600

    added Gif

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
+    user: 'julianbowman',
     password: 'montigue',
-    host: 'ec2-54-151-116-72.us-west-1.compute.amazonaws.com',
+    host: 'localhost',
     port: 5432,
     database: 'thermal_springs'
 });
